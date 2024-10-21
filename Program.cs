7cab8321105f5ba98709c9df056b483f6d4db6e2
Keveen Menezes <endykeven@gmail.com>
2024-10-21 18:06:01 -0300

    feat: adicionando as primeiras questoes

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace LeetCode;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var result = IsPalindromeQuest.IsPalindrome(121);
+        Console.WriteLine(result);
+    }
+}
